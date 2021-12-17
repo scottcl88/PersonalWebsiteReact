170da83f8695c0c3595692bee2c7a78fdcc67671
Scott Lewis <scottcl@outlook.com>
2021-12-16 20:07:23 -0500

    Initial commit part 6

--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="https://scottcl88.github.io/PersonalWebsiteReact">
+    <Router  basename={process.env.PUBLIC_URL}>
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
