bedc1b28d687df0bb8f5bc59646e6c8397a66a10
Scott Lewis <scottcl@outlook.com>
2022-01-01 14:15:29 -0500

    Resume and contact updates

--- src/app/contact/Contact.tsx
+++ src/app/contact/Contact.tsx
@@ -1,6 +1,50 @@
+import axios from 'axios';
 import React from 'react';
 
 class Contact extends React.Component {
+
+    state: any = { email: "", name: "", subject: "", message: "" };
+    constructor(props: any) {
+        super(props);
+        this.handleInputChange = this.handleInputChange.bind(this);
+      }
+    handleInputChange(event: any) {
+        const target = event.target;
+        const value = target.type === "checkbox" ? target.checked : target.value;
+        const name = target.name;
+        console.log("Setting state to: ", name, value);
+        this.setState({
+            [name]: value,
+        });
+        console.log("Result: ", this.state);
+    }
+    submitForm = (e: any) => {
+        e.preventDefault();
+        console.log("Submit Form called: ", e, this.state);
+        var form = e.currentTarget;
+        if (!form || form.checkValidity() === false) {
+          e.preventDefault();
+          e.stopPropagation();
+          console.log("Form not valid: ", form);
+          return;
+        }
+        (window as any).grecaptcha.ready(() => {
+            (window as any).grecaptcha.execute("6Lc6buAdAAAAAPHBGxLQUegsMf_ACveCrUaHqC5O", { action: 'submit' }).then((token: any) => {
+                var request = {
+                    FromEmail: this.state?.email,
+                    Name: this.state?.name,
+                    Subject: this.state?.subject,
+                    Message: this.state?.message,
+                    RecaptchaToken: token
+                };
+                axios.post("https://foodapi20210616194736.azurewebsites.net/api/Contact/SendToPersonal", request).then(res => {
+                    console.log("Form submitted: ", res);
+                }).catch(err => {
+                    console.error("Failed to submit form: ", err);
+                });
+            });
+        });
+    }
     render() {
         return (
             <div className="contact">
@@ -15,18 +59,22 @@ class Contact extends React.Component {
                                 <p className="contact-card-content"><a href="mailto:scottcl@outlook.com" target="_blank">scottcl@outlook.com</a></p>
                             </div>
                         </div>
-                        <form action="#!" className="contact-form">
+                        <form onSubmit={this.submitForm} className="contact-form">
                             <div className="form-group form-group-name">
                                 <label htmlFor="name" className="sr-only">Name</label>
-                                <input type="text" name="name" id="name" className="form-control" placeholder="NAME" />
+                                <input type="text" name="name" id="name" className="form-control" placeholder="NAME" required value={this.state?.name} onChange={this.handleInputChange} />
                             </div>
                             <div className="form-group form-group-email">
                                 <label htmlFor="email" className="sr-only">Email</label>
-                                <input type="email" name="email" id="email" className="form-control" placeholder="EMAIL" />
+                                <input type="email" name="email" id="email" className="form-control" placeholder="EMAIL" required value={this.state?.fromEmail} onChange={this.handleInputChange}/>
+                            </div>
+                            <div className="form-group form-group-subject">
+                                <label htmlFor="subject" className="sr-only">Subject</label>
+                                <input type="text" name="subject" id="subject" className="form-control" placeholder="SUBJECT" required value={this.state?.subject} onChange={this.handleInputChange}/>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="message" className="sr-only">Message</label>
-                                <textarea name="message" id="message" className="form-control" placeholder="MESSAGE" rows={5}></textarea>
+                                <textarea name="message" id="message" className="form-control" placeholder="MESSAGE" rows={5} required value={this.state?.message} onChange={this.handleInputChange}></textarea>
                             </div>
                             <button type="submit" className="btn btn-primary form-submit-btn">SEND MESSAGE</button>
                         </form>
--- src/app/resume/Resume.tsx
+++ src/app/resume/Resume.tsx
@@ -1,15 +1,58 @@
+import axios from 'axios';
 import React from 'react';
+import { Dropdown } from 'react-bootstrap';
 
 class Resume extends React.Component {
+
+    downloadPdf = (e: any, type: number) => {
+        e.preventDefault();
+        console.log("downloadPdf called: ", e);
+        (window as any).grecaptcha.ready(() => {
+            (window as any).grecaptcha.execute("6Lc6buAdAAAAAPHBGxLQUegsMf_ACveCrUaHqC5O", { action: 'submit' }).then((token: any) => {
+                axios({
+                    url: `https://foodapi20210616194736.azurewebsites.net/api/Document/DownloadResume?resumeType=${type}`,
+                    method: 'GET',
+                    responseType: 'blob',
+                    headers: { RecaptchaToken: token }
+                }).then((response) => {
+                    let filename = "";
+                    const contentDisposition = response.headers['content-disposition'];
+                    console.log("ContentDisposition", contentDisposition);
+                    if (contentDisposition && contentDisposition.indexOf('attachment') !== -1) {
+                        var filenameRegex = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
+                        var matches = filenameRegex.exec(contentDisposition);
+                        if (matches != null && matches[1]) {
+                            filename = matches[1].replace(/['"]/g, '');
+                        }
+                    }
+                    console.log("filename", filename);
+                    const contentType = response.headers['content-type'];
+                    const blob = new Blob([response.data], { type: contentType });
+                    const link = document.createElement('a');
+                    link.href = window.URL.createObjectURL(blob);
+                    link.download = filename;
+                    link.click();
+                }).catch((err: any) => {
+                    console.error("Failed to download: ", err);
+                });
+            });
+        });
+    }
     render() {
         return (
-            <div className="resume">                
-                <div className="text-center mt-4">
-                            <a className="btn btn-xl btn-outline-light" target="_blank" href="~/Public_SCLewis_Resume.pdf">
-                                <i className="fas fa-file-alt mr-2"></i>
-                                Resume
-                            </a>
-                        </div>
+            <div className="resume">
+                <Dropdown className="text-center mt-4">
+                    <Dropdown.Toggle variant="success" id="dropdown-basic">
+                        <i className="fas fa-download mr-2"></i>Download
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.Item onClick={(e) => this.downloadPdf(e, 3)}><i className="fas fa-file-pdf mr-2"></i>PDF</Dropdown.Item>
+                        <Dropdown.Item onClick={(e) => this.downloadPdf(e, 1)}><i className="fas fa-file-word mr-2"></i>DOCX</Dropdown.Item>
+                        <Dropdown.Item onClick={(e) => this.downloadPdf(e, 4)}><i className="fas fa-file-alt mr-2"></i>RTF</Dropdown.Item>
+                        <Dropdown.Item onClick={(e) => this.downloadPdf(e, 5)}><i className="fas fa-file-alt mr-2"></i>TXT</Dropdown.Item>
+                        <Dropdown.Item onClick={(e) => this.downloadPdf(e, 2)}><i className="fas fa-file-code mr-2"></i>HTML</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
                 <div className="WordSection1">
                     <p className="MsoNormal"><b><span style={{ fontSize: '16.0pt', fontFamily: '"Calibri",sans-serif', color: 'black' }}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Scott Lewis</span></b></p>
                     <p className="MsoNormal"><b><span style={{ fontSize: '11.0pt', fontFamily: '"Calibri",sans-serif', color: 'black' }}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span></b><b><span style={{ fontSize: '11.0pt', fontFamily: '"Calibri",sans-serif' }}>&nbsp;</span></b><span style={{ fontSize: '10.0pt', fontFamily: '"Calibri",sans-serif', color: '#1A1A1B', background: 'white' }}>GitHub:
