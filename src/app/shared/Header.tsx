d21e65e68335adc8c37fc813f95ca0c6500df6e5
Scott Lewis <scottcl@outlook.com>
2022-01-03 16:36:58 -0500

    Style updates with navigation and a

--- src/app/shared/Header.tsx
+++ src/app/shared/Header.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 
 class Header extends React.Component {
+
+    isPathActive(path: any) {
+        return (window).location.pathname.startsWith(path);
+    }
     render() {
         return (
             <nav className="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
@@ -13,10 +17,10 @@ class Header extends React.Component {
                     </button>
                     <div className="collapse navbar-collapse" id="navbarResponsive">
                         <ul className="navbar-nav ml-auto">
-                            <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="/portfolio">Portfolio</a></li>
-                            <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="/resume">Resume</a></li>
-                            <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="/about">About</a></li>
-                            <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="/contact">Contact</a></li>
+                            <li className="nav-item mx-0 mx-lg-1"><a className={this.isPathActive("/portfolio") ? "nav-link active py-3 px-0 px-lg-3" : "nav-link py-3 px-0 px-lg-3"} href="/portfolio">Portfolio</a></li>
+                            <li className="nav-item mx-0 mx-lg-1"><a className={this.isPathActive("/resume") ? "nav-link active py-3 px-0 px-lg-3" : "nav-link py-3 px-0 px-lg-3"} href="/resume">Resume</a></li>
+                            <li className="nav-item mx-0 mx-lg-1"><a className={this.isPathActive("/about") ? "nav-link active py-3 px-0 px-lg-3" : "nav-link py-3 px-0 px-lg-3"} href="/about">About</a></li>
+                            <li className="nav-item mx-0 mx-lg-1"><a className={this.isPathActive("/contact") ? "nav-link active py-3 px-0 px-lg-3" : "nav-link py-3 px-0 px-lg-3"} href="/contact">Contact</a></li>
                         </ul>
                     </div>
                 </div>
